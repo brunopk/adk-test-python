9b4756d643488bfd4016c9e3c27a8e1bfd462502
Bruno <brunopiaggiok@gmail.com>
2025-11-26 10:09:36 -0300

    Initial structure created with uvx as described in https://google.github.io/adk-docs/deploy/agent-engine/\#prepare-ad

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -12,3 +12,7 @@ root_agent = Agent(
     instruction="You are a helpful assistant that tells the current time in cities. Use the 'get_current_time' tool for this purpose.",
     tools=[get_current_time],
 )
+
+from google.adk.apps.app import App
+
+app = App(root_agent=root_agent, name="app")
